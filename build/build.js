3e4fdc2f0cddccf7adfe9c1cfffcccfa2dab967e
JulesFouchy <45451201+JulesFouchy@users.noreply.github.com>
2021-02-12 21:36:10 +0100

    Add files via upload

---
+++ build/build.js
@@ -0,0 +1,74 @@
+function draw() {
+    var R = height / 4;
+    var r = R / 2;
+    background('white');
+    noStroke();
+    fill('black');
+    ellipse(width / 2, height / 2, R * 2);
+    {
+        var p = p5.Vector.fromAngle(-TAU / 4 + 0.115 * TAU).mult(R + r * 0.9);
+        ellipse(width / 2 + p.x, height / 2 + p.y, r * 2);
+    }
+    {
+        var p = p5.Vector.fromAngle(-TAU / 4 - 0.115 * TAU).mult(R + r * 0.9);
+        ellipse(width / 2 + p.x, height / 2 + p.y, r * 2);
+    }
+}
+function setup() {
+    p6_CreateCanvas();
+}
+function windowResized() {
+    p6_ResizeCanvas();
+}
+var __ASPECT_RATIO = 1;
+var __MARGIN_SIZE = 25;
+function __desiredCanvasWidth() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return windowWidth - __MARGIN_SIZE * 2;
+    }
+    else {
+        return __desiredCanvasHeight() * __ASPECT_RATIO;
+    }
+}
+function __desiredCanvasHeight() {
+    var windowRatio = windowWidth / windowHeight;
+    if (__ASPECT_RATIO > windowRatio) {
+        return __desiredCanvasWidth() / __ASPECT_RATIO;
+    }
+    else {
+        return windowHeight - __MARGIN_SIZE * 2;
+    }
+}
+var __canvas;
+function __centerCanvas() {
+    __canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+}
+function p6_CreateCanvas() {
+    __canvas = createCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+function p6_ResizeCanvas() {
+    resizeCanvas(__desiredCanvasWidth(), __desiredCanvasHeight());
+    __centerCanvas();
+}
+var p6_SaveImageSequence = function (durationInFrames, fileExtension) {
+    if (frameCount <= durationInFrames) {
+        noLoop();
+        var filename_1 = nf(frameCount - 1, ceil(log(durationInFrames) / log(10)));
+        var mimeType = (function () {
+            switch (fileExtension) {
+                case 'png':
+                    return 'image/png';
+                case 'jpeg':
+                case 'jpg':
+                    return 'image/jpeg';
+            }
+        })();
+        __canvas.elt.toBlob(function (blob) {
+            p5.prototype.downloadFile(blob, filename_1, fileExtension);
+            setTimeout(function () { return loop(); }, 100);
+        }, mimeType);
+    }
+};
+//# sourceMappingURL=../src/src/build.js.map
